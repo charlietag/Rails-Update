24711dc54583f586e4599b68e361f22c8d24fb11
Charlie Hsieh <charlieofficial@gmail.com>
2016-06-17 08:17:03 +0800

    Learn Ruby on Rails CRUD - Update

---
+++ db/migrate/20160313065149_create_destinations.rb
@@ -0,0 +1,13 @@
+class CreateDestinations < ActiveRecord::Migration
+  def change
+    create_table :destinations do |t|
+
+      t.string :name
+      t.string :image
+      t.string :description
+      t.timestamps null: false
+
+      t.references :tag
+    end
+  end
+end
